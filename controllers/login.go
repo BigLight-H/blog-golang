180c2c734b269e26b4c497b17293bca20a586d21
黄光日 <463817374@qq.com>
2019-08-29 17:49:34 +0800

    注册

--- controllers/login.go
+++ controllers/login.go
@@ -38,11 +38,41 @@ func (this *LoginController) Login()	 {
 
 }
 
+//前台登陆
 func (this *LoginController) HomeLogin() {
+	username := this.GetString("username")
+	password := this.GetString("password")
+	user := models.Client{Username:username}
+	this.o.Read(&user,"username")
+	if user.Password == "" {
+		this.MsgBack("账号不存在",0)
+	}
 
+	if util.Md5(password) != strings.Trim(user.Password, " ") {
+		this.MsgBack("密码错误", 0)
+	} else {
+		data := models.Client{}
+		qs := this.o.QueryTable(new(models.User).TableName())
+		qs.Filter("id", user.Id).RelatedSel().All(&data)
+		this.SetSession("client", data)
+		this.SetSession("client_id", data.Id)
+		this.MsgBack("登录成功", 1)
+	}
 }
 
 func (this *LoginController) Register()  {
+	username := this.GetString("username")
+	password := this.GetString("password")
+	password1 := this.GetString("password1")
+	if password != password1 {
+		this.MsgBack("两次密码不一致", 0)
+	} else if len(username) < 2 {
+		this.MsgBack("用户名长度不能小于2", 0)
+	} else if len(password) < 6 {
+		this.MsgBack("用户名长度不能小于6", 0)
+	} else {
+
+	}
 
 }
 
