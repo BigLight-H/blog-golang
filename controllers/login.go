ce90808372830ca3f61ca766c3017a26effd6a6e
黄光日 <463817374@qq.com>
2019-08-30 15:58:30 +0800

    注册添加默认头像

--- controllers/login.go
+++ controllers/login.go
@@ -4,6 +4,7 @@ import (
 	"beego-demo/models"
 	"beego-demo/util"
 	"github.com/davecgh/go-spew/spew"
+	"strconv"
 	"strings"
 )
 
@@ -62,12 +63,15 @@ func (this *LoginController) HomeLogin() {
 	}
 }
 
+//注册前台用户
 func (this *LoginController) Register()  {
 	email := this.GetString("email")
 	username := this.GetString("uname")
 	mobile := this.GetString("mobile")
 	password := this.GetString("pwd")
 	password1 := this.GetString("pwd_")
+	id_ := this.GetString("id")
+	id, _ := strconv.Atoi(id_)
 	if password != password1 {
 		this.MsgBack("两次密码不一致", 0)
 	} else if len(username) < 2 {
@@ -82,16 +86,35 @@ func (this *LoginController) Register()  {
 		data := models.Client{}
 		data.Email = email
 		data.Mobile = mobile
-		data.Password = password
+		data.Password = util.Md5(password)
 		data.Username = username
+		data.Sex = 1
+		img := models.HeadImg{Id:id}
+		data.HeadImg = &img
 		_, err := this.o.Insert(&data)
 		if err != nil {
+			spew.Dump(err)
 			this.MsgBack("注册失败", 0)
 		}
 		this.MsgBack("注册成功", 1)
 	}
 }
 
+//找回密码
 func (this *LoginController) Retrieve() {
-
+	email := this.GetString("email")
+	if this.VerifyEmailFormat(email) == false {
+		this.MsgBack("邮箱号码格式不对", 0)
+	} else {
+		user := models.Client{Email:email}
+		spew.Dump(user)
+		this.o.Read(&user,"Email")
+		if user.Password == "" {
+			this.MsgBack("账号不存在",0)
+		} else {
+			//查看邮件验证是否存在
+			//发送邮件验证码
+			//对比邮件验证码
+		}
+	}
 }
